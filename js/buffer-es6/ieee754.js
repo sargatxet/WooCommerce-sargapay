46af2e1853aaff27e5c1c51021ec6aa1f0d23ccf
Toni Ramiro <sargatxet@gmail.com>
2021-11-30 23:55:03 +0100

    Set LICENSE to all code files

--- js/buffer-es6/ieee754.js
+++ js/buffer-es6/ieee754.js
@@ -1,30 +1,47 @@
+/*
+    SargaPay. Cardano gateway plug-in for Woocommerce. 
+    Copyright (C) 2021  Sargatxet Pools
 
-export function read (buffer, offset, isLE, mLen, nBytes) {
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+export function read(buffer, offset, isLE, mLen, nBytes) {
   var e, m
   var eLen = nBytes * 8 - mLen - 1
   var eMax = (1 << eLen) - 1
   var eBias = eMax >> 1
   var nBits = -7
-  var i = isLE ? (nBytes - 1) : 0
+  var i = isLE ? nBytes - 1 : 0
   var d = isLE ? -1 : 1
   var s = buffer[offset + i]
 
   i += d
 
-  e = s & ((1 << (-nBits)) - 1)
-  s >>= (-nBits)
+  e = s & ((1 << -nBits) - 1)
+  s >>= -nBits
   nBits += eLen
   for (; nBits > 0; e = e * 256 + buffer[offset + i], i += d, nBits -= 8) {}
 
-  m = e & ((1 << (-nBits)) - 1)
-  e >>= (-nBits)
+  m = e & ((1 << -nBits) - 1)
+  e >>= -nBits
   nBits += mLen
   for (; nBits > 0; m = m * 256 + buffer[offset + i], i += d, nBits -= 8) {}
 
   if (e === 0) {
     e = 1 - eBias
   } else if (e === eMax) {
-    return m ? NaN : ((s ? -1 : 1) * Infinity)
+    return m ? NaN : (s ? -1 : 1) * Infinity
   } else {
     m = m + Math.pow(2, mLen)
     e = e - eBias
@@ -32,13 +49,13 @@ export function read (buffer, offset, isLE, mLen, nBytes) {
   return (s ? -1 : 1) * m * Math.pow(2, e - mLen)
 }
 
-export function write (buffer, value, offset, isLE, mLen, nBytes) {
+export function write(buffer, value, offset, isLE, mLen, nBytes) {
   var e, m, c
   var eLen = nBytes * 8 - mLen - 1
   var eMax = (1 << eLen) - 1
   var eBias = eMax >> 1
-  var rt = (mLen === 23 ? Math.pow(2, -24) - Math.pow(2, -77) : 0)
-  var i = isLE ? 0 : (nBytes - 1)
+  var rt = mLen === 23 ? Math.pow(2, -24) - Math.pow(2, -77) : 0
+  var i = isLE ? 0 : nBytes - 1
   var d = isLE ? 1 : -1
   var s = value < 0 || (value === 0 && 1 / value < 0) ? 1 : 0
 
@@ -75,11 +92,19 @@ export function write (buffer, value, offset, isLE, mLen, nBytes) {
     }
   }
 
-  for (; mLen >= 8; buffer[offset + i] = m & 0xff, i += d, m /= 256, mLen -= 8) {}
+  for (
+    ;
+    mLen >= 8;
+    buffer[offset + i] = m & 0xff, i += d, m /= 256, mLen -= 8
+  ) {}
 
   e = (e << mLen) | m
   eLen += mLen
-  for (; eLen > 0; buffer[offset + i] = e & 0xff, i += d, e /= 256, eLen -= 8) {}
+  for (
+    ;
+    eLen > 0;
+    buffer[offset + i] = e & 0xff, i += d, e /= 256, eLen -= 8
+  ) {}
 
   buffer[offset + i - d] |= s * 128
 }
--- js/buffer-es6/isArray.js
+++ js/buffer-es6/isArray.js
@@ -1,5 +1,23 @@
-var toString = {}.toString;
+/*
+    SargaPay. Cardano gateway plug-in for Woocommerce. 
+    Copyright (C) 2021  Sargatxet Pools
 
-export default Array.isArray || function (arr) {
-  return toString.call(arr) == '[object Array]';
-};
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+var toString = {}.toString
+
+export default Array.isArray ||
+  function (arr) {
+    return toString.call(arr) == "[object Array]"
+  }
