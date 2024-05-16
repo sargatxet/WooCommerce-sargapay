afad06d712be951f4c9dc44c473de29aacda2865
TrakaDev <trakadev@gmail.com>
2024-05-16 11:02:05 -0600

    setup dependencies for wordpress blocks

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,51 @@
  * WordPress Dependencies
  */
 const defaultConfig = require('@wordpress/scripts/config/webpack.config.js');
+const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
+const path = require('path');
+
+const wcDepMap = {
+    '@woocommerce/blocks-registry': ['wc', 'wcBlocksRegistry'],
+    '@woocommerce/settings': ['wc', 'wcSettings']
+};
+
+const wcHandleMap = {
+    '@woocommerce/blocks-registry': 'wc-blocks-registry',
+    '@woocommerce/settings': 'wc-settings'
+};
+
+const requestToExternal = (request) => {
+    if (wcDepMap[request]) {
+        return wcDepMap[request];
+    }
+};
+
+const requestToHandle = (request) => {
+    if (wcHandleMap[request]) {
+        return wcHandleMap[request];
+    }
+};
 
 module.exports = {
     ...defaultConfig,
     ... {
+        entry: {
+            'frontend/blocks': '/resources/js/frontend/index.js',
+        },
+        output: {
+            path: path.resolve(__dirname, 'assets/js'),
+            filename: '[name].js',
+        },
+        plugins: [
+            ...defaultConfig.plugins.filter(
+                (plugin) =>
+                    plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+            ),
+            new WooCommerceDependencyExtractionWebpackPlugin({
+                requestToExternal,
+                requestToHandle
+            })
+        ],
         // Add any overrides to the default here.        
         experiments: {
             // WebAssembly as async module (Proposal)       
